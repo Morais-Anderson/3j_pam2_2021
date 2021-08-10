a86b1fb06fb2031e7caaac7f733500ef1125d518
Morais-Anderson <anderson_moraism@hotmail.com>
2021-08-10 20:43:46 -0300

    Gitignore criacao

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ servidor.get(
     '/', (req, res, next) =>{
         return res.send(
             {
-                "mensagem": "Bem-vindo(a) ao servidor"
+                "mensagem": "Bem-vindo(a) ao servidor",
+                "cidade": "Itapeva",
+                "uf":"SP"
             }
         )
     }
